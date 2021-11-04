bff69aca206d02f4477856d87dabad02f6b71fea
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-11-04 10:04:13 -0500

    feat:  Formulario feature added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, {Fragment} from 'react';
+import React, {createContext,useState} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
-
+import CategoriasProvider from './context/CategoriasContext';
 
 function App() {
   return (
- <Fragment>
+ <CategoriasProvider>
    <Header/>
 
    <div className="container mt-5">
    <div className="row">
-
+<Formulario/>
    </div>
    </div>
- </Fragment>
+ </CategoriasProvider>
   );
 }
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { CategoriasContext } from "../context/CategoriasContext";
 
 const Formulario = () => {
-    return (
-       <h1>Formulario aqui</h1>
-    )
-}
 
-export default Formulario
+
+
+const {categorias} = useContext(CategoriasContext);
+
+
+
+
+  return (
+    <form className="col-md-12">
+      <fieldset className="text-center">
+        <legend>Busca bebidas por categoria o Ingrediente</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            name="nombre"
+            className="form-control"
+            type="text"
+            placeholder="Buscar por Ingrendiente"
+          />
+        </div>
+
+        <div className="col-md-4">
+          <select className="form-control" name="categoria">
+            <option value="">-- Selecciona Categoria--</option>
+
+{categoria.map(categoria =>(
+
+<option
+ key={categoria.strCategory}
+ value={categoria.strCategory}
+/>
+
+) )}
+
+
+          </select>
+        </div>
+
+        <div className="col-md-4">
+          <input type="submit" className="btn btn-block btn-primary"
+          value="Buscar Bebidas" />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
