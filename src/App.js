c1faa7ef9437341ac18419426dd607ead65f814c
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-11-05 07:30:43 -0500

    feat: Content modal added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, {createContext,useState} from 'react';
-import Header from './components/Header';
-import Formulario from './components/Formulario';
-import ListaRecetas from './components/ListaRecetas';
+import React, { createContext, useState } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListaRecetas from "./components/ListaRecetas";
 
-import CategoriasProvider from './context/CategoriasContext';
-import RecetasProvider from './context/RecetasContext';
+import CategoriasProvider from "./context/CategoriasContext";
+import RecetasProvider from "./context/RecetasContext";
+import ModalProvider from "./context/ModalContext";
 
 function App() {
   return (
- <CategoriasProvider>
-   <RecetasProvider>
-   <Header/>
+    <CategoriasProvider>
+      <RecetasProvider>
+        <ModalProvider>
+          <Header />
 
-   <div className="container mt-5">
-   <div className="row">
-<Formulario/>
-   </div>
+          <div className="container mt-5">
+            <div className="row">
+              <Formulario />
+            </div>
 
-   <ListaRecetas />
-
-
-
-   
-   </div>
-   </RecetasProvider>
- </CategoriasProvider>
+            <ListaRecetas />
+          </div>
+        </ModalProvider>
+      </RecetasProvider>
+    </CategoriasProvider>
   );
 }
 
--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React,{useContext, useState} from "react";
+import { ModalContext } from "../context/ModalContext";
+
+
+
 
 const Receta = ({ receta }) => {
+
+
+// Extraer los valores del context
+const {guardarIdReceta} = useContext(ModalContext);
+
+    
   return (
     <div className="col -md-4 mb-3">
       <div className="card">
@@ -16,6 +26,9 @@ const Receta = ({ receta }) => {
           <button
             type="button"
             className="btn btn-block btn-primary"
+            onClick={()=> {
+              guardarIdReceta(receta.idDrink)
+            }}
           
           
           >Ver Receta</button>
