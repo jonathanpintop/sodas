4fdc7c1d9dfaee71e6ef1ca55ffc02a27d9cde4e
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-11-05 09:31:08 -0500

    feat: Modal feature added

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,16 +1,66 @@
-import React,{useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
 
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 450,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Receta = ({ receta }) => {
+  // Configuración del modal de material
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
 
+  const classes = useStyles();
 
-// Extraer los valores del context
-const {guardarIdReceta} = useContext(ModalContext);
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // Extraer los valores del context
+  const { informacion, guardarIdReceta, guardarReceta } =
+    useContext(ModalContext);
+
+  // Muestra y formatea los ingredientes
+
+  const mostrarIngredientes = (informacion) => {
+    let ingredientes = [];
+    for (let i = 1; i < 16; i++) {
+      if (informacion[`strIngredient${i}`]) {
+        ingredientes.push(
+          <li>
+            {" "}
+            {informacion[`strIngredient${i}`]} {informacion[`strMeasure${i}`]}
+          </li>
+        );
+      }
+    }
+
+    return ingredientes;
+  };
+
   return (
     <div className="col -md-4 mb-3">
       <div className="card">
@@ -26,12 +76,33 @@ const {guardarIdReceta} = useContext(ModalContext);
           <button
             type="button"
             className="btn btn-block btn-primary"
-            onClick={()=> {
-              guardarIdReceta(receta.idDrink)
+            onClick={() => {
+              guardarIdReceta(receta.idDrink);
+              handleOpen();
             }}
-          
-          
-          >Ver Receta</button>
+          >
+            Ver Receta
+          </button>
+
+          <Modal
+            open={open}
+            onClose={() => {
+              guardarIdReceta(null);
+              guardarReceta({});
+              handleClose();
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h2>{informacion.strDrink}</h2>
+              <h3 className="mt-4">Instrucciones</h3>
+              <p>{informacion.strInstructions}</p>
+
+              <img className="img-fluid my-4" src={informacion.strDrinkThumb} />
+
+              <h3>Ingredientes y cantidades</h3>
+              <ul>{mostrarIngredientes(informacion)}</ul>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -7,11 +7,33 @@ export const ModalContext = createContext();
 const ModalProvider = (props) => {
   // state del provider
   const [idreceta, guardarIdReceta] = useState(null);
+  const [informacion, guardarReceta] = useState({});
+
+  // Una vez que tenemos una receta , llamar la api
+
+  useEffect(() => {
+    const obtenerReceta = async () => {
+      if (!idreceta) return;
+
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+      const resultado = await axios.get(url);
+
+      guardarReceta(resultado.data.drinks[0]);
+    };
+    obtenerReceta();
+  }, [idreceta]);
 
   return (
-    <ModalContext.Provider value={{
-        guardarIdReceta
-    }}>{props.children}</ModalContext.Provider>
+    <ModalContext.Provider
+      value={{
+        informacion,
+        guardarIdReceta,
+        guardarReceta,
+      }}
+    >
+      {props.children}
+    </ModalContext.Provider>
   );
 };
 
